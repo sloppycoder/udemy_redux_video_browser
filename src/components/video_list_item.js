8099143380fda226fdb5e2bcf4755e8a59b505fe
Li Lin <guru.lin@gmail.com>
2016-06-04 15:13:54 +0800

    added ability to select video from list

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const VideoListItem = ({ video }) => {
+const VideoListItem = ({ video, onVideoSelect }) => {
   const snippet = video.snippet;
   const imageUrl = snippet.thumbnails.default.url;
   return (
-    <li className="list-group-item">
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
           <img className="media-object" role="presentation" src={imageUrl} />
