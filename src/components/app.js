6e8ee9e22e9c2014eb1cb09e97f5a225c158e84d
Li Lin <guru.lin@gmail.com>
2016-06-04 14:39:01 +0800

    added video detail

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './search_bar';
 import VideoList from './video_list';
+import VideoDetail from './video_detail';
 
 const API_KEY = 'AIzaSyBSW9Oi7ffwrkDH58hVaxTBBjYbJv2iuLE';
 
@@ -20,6 +21,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
         <VideoList videos={this.state.videos} />
       </div>
     );
